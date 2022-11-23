4f14b17b5fa786bd90a0ea22bc9ce600919bbfd7
Tarun <tarsuno@gmail.com>
2022-11-23 09:23:13 +0100

    Add data folder with env variable

--- checkpoint_sync/chk_prysm.sh
+++ checkpoint_sync/chk_prysm.sh
@@ -1,2 +1,2 @@
-sudo docker run --network=host -v $PWD/apps-data/.prysm:/home/.eth2/ gcr.io/prysmaticlabs/prysm/beacon-chain:v3.1.1 --checkpoint-sync-url="$1" --genesis-beacon-api-url="$1" --accept-terms-of-use
+sudo docker run --network=host -v ${PRYSM_DATA_FOLDER}.prysm:/home/.eth2/ gcr.io/prysmaticlabs/prysm/beacon-chain:v3.1.1 --checkpoint-sync-url="$1" --genesis-beacon-api-url="$1" --accept-terms-of-use
 
