4f17982416092f87396b1c58e91dfd18856366d6
Tarun <tarsuno@gmail.com>
2022-12-21 12:11:58 +0000

    Fix invalid permissions

--- checkpoint_sync/chk_nimbus.sh
+++ checkpoint_sync/chk_nimbus.sh
@@ -1,4 +1,4 @@
 export $(grep -v '^#' .env | xargs)
-sudo docker run --network=host -v ${NIMBUS_DATA_FOLDER}/.nimbus:/home/user/.cache/nimbus/BeaconNode statusim/nimbus-eth2:amd64-v22.9.1 trustedNodeSync \
+sudo docker run --network=host -v ${NIMBUS_DATA_FOLDER}/.nimbus:/home/user/.cache/nimbus/ statusim/nimbus-eth2:amd64-v22.9.1 trustedNodeSync \
   --network:mainnet \
   --trusted-node-url="$1"
