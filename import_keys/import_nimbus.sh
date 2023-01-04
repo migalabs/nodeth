05dd45e495c78afc0b90600d19b0474bb8d270bc
Tarun <tarsuno@gmail.com>
2023-01-04 15:51:35 +0000

    Change nimbus import

--- import_keys/import_nimbus.sh
+++ import_keys/import_nimbus.sh
@@ -1,3 +1,4 @@
 export $(grep -v '^#' .env | xargs)
 
-sudo cp -r ${VALIDATOR_KEYS_FOLDER}/nimbus/* ${NIMBUS_DATA_FOLDER}/.nimbus
+sudo cp -r ${VALIDATOR_KEYS_FOLDER}/nimbus/secrets ${NIMBUS_DATA_FOLDER}/.nimbus
+sudo cp -r ${VALIDATOR_KEYS_FOLDER}/nimbus/validators ${NIMBUS_DATA_FOLDER}/.nimbus
