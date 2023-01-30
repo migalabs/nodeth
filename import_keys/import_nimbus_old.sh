a0b988a30f4aaba9036361a35d171616b5e706b6
Tarun <tarsuno@gmail.com>
2023-01-30 08:22:17 +0100

    Adjust  folder permission to validator import

--- import_keys/import_nimbus_old.sh
+++ import_keys/import_nimbus_old.sh
@@ -1,5 +1,5 @@
 export $(grep -v '^#' .env | xargs)
-
+sudo chown -R $1:$1 ${VALIDATOR_KEYS_FOLDER}/nimbus
 
 for d in ${VALIDATOR_KEYS_FOLDER}/nimbus/validators/*; do
 	sudo cat ${VALIDATOR_KEYS_FOLDER}/nimbus/secret.txt | tee | sudo docker run --rm -i --network=host \
