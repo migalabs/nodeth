63640cfe90c8339b246a7fe42a50ec44acc0cdb2
Tarun <tarsuno@gmail.com>
2023-01-30 08:22:02 +0100

    Adjust folder permission to checkpoint sync

--- checkpoint_sync/chk_teku.sh
+++ checkpoint_sync/chk_teku.sh
@@ -1,2 +1,4 @@
 export $(grep -v '^#' .env | xargs)
+sudo mkdir -p ${TEKU_DATA_FOLDER}/.teku
+sudo chown -R $2:$2 ${TEKU_DATA_FOLDER}/.teku
 sudo docker run --rm --network=host -v ${TEKU_DATA_FOLDER}/.teku:/opt/teku/.local/share/teku/ consensys/teku:${TEKU_TAG} --network=${NETWORK} --initial-state="$1/eth/v2/debug/beacon/states/finalized" --ee-endpoint http://127.0.0.1:8551
