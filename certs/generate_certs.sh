d3fc40ff22e25d1f653fea5419eee71d423782fd
Tarun <tarsuno@gmail.com>
2023-12-28 15:13:31 +0100

    Remove unneeded flag, improve certificate generation

--- certs/generate_certs.sh
+++ certs/generate_certs.sh
@@ -1,1 +1,1 @@
-openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out cert.pem
+openssl req -newkey rsa:2048 -nodes -keyout node.key -x509 -days 365 -out cert.crt -subj "/CN=yourip" -addext "subjectAltName=IP:yourip"
